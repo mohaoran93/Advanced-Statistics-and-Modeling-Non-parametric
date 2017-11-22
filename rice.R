8325f99d81d0bb3bdb5e197ae36d8757f48aac57
mohaoran <m.haoran@innopolis.ru>
2017-11-22 20:40:35 +0100

    Merged two functions
    Merged two functions into one as the task required.

--- rice.R
+++ rice.R
@@ -1,6 +1,6 @@
 # Rice
 # Input: 
-#      x,y  vectors
+#      X,Y  vectors
 
 #
 # Output:  
@@ -8,13 +8,48 @@
 # 
 
 
-rice <- function(x,y){
-  df <- data.frame(x,y)
-  df = df[order(df[,'x']),]
-  t2 = df[-c(length(ROOM)),"y"]
-  t1 = df[-c(1),"y"]
+rice <- function(X,Y){
+  XY <- data.frame(X,Y)
+  XY = XY[order(XY[,'X']),]
+  t2 = XY[-c(length(X)),"Y"]
+  t1 = XY[-c(1),"Y"]
   sigma_2 <- (sum((t2-t1)^2))/(2*(length(ROOM)-1))
-  return(sigma_2)  
+  
+  summation <- 0
+  n <- length(Y)
+  y.hat <- c()
+  residual.hat <- c()
+  
+  for (i in 2:(n-1)) {
+    xi <- XY[i, 'X']
+    
+    x.previous = XY[i-1,'X'] # x_i-1
+    x.following = XY[i+1,'X'] # x_i+1
+    y.previous = XY[i-1,'Y'] # y_i-1
+    y.following = XY[i+1,'Y'] # y_i+1
+    
+    rep <- table(X)
+    repetitions <- rep[names(rep)==xi]
+    
+    if (repetitions >= 3) {
+      xui <- min(XY[XY$X > xi,])
+      xli <- max(XY[XY$X < xi,])
+    } else {
+      xui <- x.following
+      xli <- x.previous
+    }
+    
+    ai <- (x.following - xi)/(xui - xli)
+    bi <- (xi - x.previous)/(xui - xli)
+    
+    y.hat[i] <- ai*y.previous + bi*y.following
+    
+    residual.hat[i] <- y.hat[i] - XY[i, 'Y']
+    summation <- summation + (residual.hat[i]^2/(ai^2 + bi^2 + 1))
+  }
+  
+  residual.variance <- summation/(n - 2)
+  return(list(sigma_2,residual.variance))  
 }
 #bos.fit.loess <- loess(ROOM ~ LSTAT, boston.c)
 #bos.fit.sm <- sm.regression(LSTAT,ROOM)
