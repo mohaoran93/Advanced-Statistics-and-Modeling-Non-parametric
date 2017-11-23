6ec64964a1501afee75584142230fe2a7b611f1c
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2017-11-23 15:39:55 +0100

    Fixed some issues in the second method

--- rice.R
+++ rice.R
@@ -7,18 +7,16 @@
 #      The estimation of sigma^2 using both methods.
 # 
 
-
-rice <- function(X,Y){
+rice <- function(X,Y) {
   XY <- data.frame(X,Y)
-  XY = XY[order(XY[,'X']),]
+  XY <- XY[order(XY$X),]
+  rownames(XY) <- 1:nrow(XY)
   t2 = XY[-c(length(X)),"Y"]
   t1 = XY[-c(1),"Y"]
   sigma_2 <- (sum((t2-t1)^2))/(2*(length(ROOM)-1))
   
   summation <- 0
   n <- length(Y)
-  y.hat <- c()
-  residual.hat <- c()
   
   for (i in 2:(n-1)) {
     xi <- XY[i, 'X']
@@ -28,28 +26,26 @@ rice <- function(X,Y){
     y.previous = XY[i-1,'Y'] # y_i-1
     y.following = XY[i+1,'Y'] # y_i+1
     
-    rep <- table(X)
-    repetitions <- rep[names(rep)==xi]
-    
-    if (repetitions >= 3) {
-      xui <- min(XY[XY$X > xi,])
-      xli <- max(XY[XY$X < xi,])
+    if (x.previous == x.following) {
+      xu_i <- min(XY[XY$X > xi,]$X)
+      xl_i <- max(XY[XY$X < xi,]$X)
     } else {
-      xui <- x.following
-      xli <- x.previous
+      xu_i <- x.following
+      xl_i <- x.previous
     }
     
-    ai <- (x.following - xi)/(xui - xli)
-    bi <- (xi - x.previous)/(xui - xli)
+    a_i <- (x.following - xi)/(xu_i - xl_i)
+    b_i <- (xi - x.previous)/(xu_i - xl_i)
     
-    y.hat[i] <- ai*y.previous + bi*y.following
+    y.hat_i <- a_i*y.previous + b_i*y.following
     
-    residual.hat[i] <- y.hat[i] - XY[i, 'Y']
-    summation <- summation + (residual.hat[i]^2/(ai^2 + bi^2 + 1))
+    residual.hat_i <- y.hat_i - XY[i, 'Y']
+    summation <- summation + (residual.hat_i^2/(a_i^2 + b_i^2 + 1))
   }
   
   residual.variance <- summation/(n - 2)
-  return(list(sigma_2,residual.variance))  
+  return(residual.variance)
 }
+
 #bos.fit.loess <- loess(ROOM ~ LSTAT, boston.c)
 #bos.fit.sm <- sm.regression(LSTAT,ROOM)
