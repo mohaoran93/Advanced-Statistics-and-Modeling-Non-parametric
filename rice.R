94c0aa718ec553b7dc0c88d7b36654e546f02ace
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2017-11-23 15:53:57 +0100

    Fixed gasser method

--- rice.R
+++ rice.R
@@ -7,16 +7,19 @@
 #      The estimation of sigma^2 using both methods.
 # 
 
-rice <- function(X,Y) {
+calculate_residual_variance <- function(X,Y) {
   XY <- data.frame(X,Y)
   XY <- XY[order(XY$X),]
   rownames(XY) <- 1:nrow(XY)
+  n <- length(Y)
+  
+  # Rice
   t2 = XY[-c(length(X)),"Y"]
   t1 = XY[-c(1),"Y"]
-  sigma_2 <- (sum((t2-t1)^2))/(2*(length(ROOM)-1))
+  rice.sigma_2 <- (sum((t2-t1)^2))/(2*(n-1))
   
+  # Gasser, Sroka, and Jennen-Steinmetz
   summation <- 0
-  n <- length(Y)
   
   for (i in 2:(n-1)) {
     xi <- XY[i, 'X']
@@ -27,15 +30,12 @@ rice <- function(X,Y) {
     y.following = XY[i+1,'Y'] # y_i+1
     
     if (x.previous == x.following) {
-      xu_i <- min(XY[XY$X > xi,]$X)
-      xl_i <- max(XY[XY$X < xi,]$X)
-    } else {
-      xu_i <- x.following
-      xl_i <- x.previous
+      x.following <- min(XY[XY$X > xi,]$X)
+      x.previous <- max(XY[XY$X < xi,]$X)
     }
     
-    a_i <- (x.following - xi)/(xu_i - xl_i)
-    b_i <- (xi - x.previous)/(xu_i - xl_i)
+    a_i <- (x.following - xi)/(x.following - x.previous)
+    b_i <- (xi - x.previous)/(x.following - x.previous)
     
     y.hat_i <- a_i*y.previous + b_i*y.following
     
@@ -43,8 +43,8 @@ rice <- function(X,Y) {
     summation <- summation + (residual.hat_i^2/(a_i^2 + b_i^2 + 1))
   }
   
-  residual.variance <- summation/(n - 2)
-  return(residual.variance)
+  gasser.sigma2 <- summation/(n - 2)
+  return(list(rice.sigma_2, gasser.sigma2))  
 }
 
 #bos.fit.loess <- loess(ROOM ~ LSTAT, boston.c)
